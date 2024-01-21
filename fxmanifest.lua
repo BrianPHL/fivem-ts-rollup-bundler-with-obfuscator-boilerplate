176ecc44cbcd876697188ff63d4e870b2b4d1e5d
Brian <brianpasco1206@gmail.com>
2024-01-21 15:15:52 +0800

    feat(boilerplate): initial files commit

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'fivem-ts-rollup-bundler-with-obfuscator-boilerplate'
+author 'brianph (https://github.com/BrianPHL)'
+
+client_script 'dist/client.bundle.js'
+server_script 'dist/server.bundle.js'
+
+provide 'boilerplate'
---
+++ rollup.config.js
@@ -0,0 +1,58 @@
+// NOTE: This file is only used for development purposes. It is not used in production.
+// This file is used to bundle the JAFMS core resource for use in the "JAFMS" server.
+
+import typescript from "@rollup/plugin-typescript"
+import multiEntry from "@rollup/plugin-multi-entry"
+import { obfuscator } from "rollup-obfuscator"
+
+const obfuscatorOptions = {
+    compact: false,
+    controlFlowFlattening: true,
+    controlFlowFlatteningThreshold: 0.5,
+    deadCodeInjection: true,
+    deadCodeInjectionThreshold: 0.2,
+    debugProtectionInterval: 0,
+    identifierNamesGenerator: 'hexadecimal',
+    numbersToExpressions: true,
+    simplify: true,
+    splitStrings: true,
+    splitStringsChunkLength: 5,
+    stringArray: true,
+    stringArrayCallsTransform: true,
+    stringArrayCallsTransformThreshold: 0.5,
+    stringArrayEncoding: ['base64'],
+    stringArrayIndexShift: true,
+    stringArrayRotate: true,
+    stringArrayShuffle: true,
+    stringArrayWrappersCount: 2,
+    stringArrayWrappersChainedCalls: true,
+    stringArrayWrappersParametersMaxCount: 4,
+    stringArrayWrappersType: 'function',
+    stringArrayThreshold: 0.5,
+    transformObjectKeys: true
+}
+
+export default [
+    {
+        input: 'src/client/*.ts',
+        output: { file: 'dist/client.bundle.js' },
+        plugins: [
+            typescript({
+                tsconfig: "src/client/tsconfig.json"
+            }),
+            multiEntry(),
+            obfuscator(obfuscatorOptions)
+        ]
+    },
+    {
+        input: 'src/server/*.ts',
+        output: { file: 'dist/server.bundle.js' },
+        plugins: [
+            typescript({
+                tsconfig: "src/server/tsconfig.json"
+            }),
+            multiEntry(),
+            obfuscator(obfuscatorOptions)
+        ]
+    }
+]
